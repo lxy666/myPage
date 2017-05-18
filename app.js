13e9a9ed477fb000811a04c62981b68102c03fd6
yu <1175058580@qq.com>
2017-05-18 20:12:37 +0800

    fix a bug

--- app.js
+++ app.js
@@ -1,28 +1,28 @@
-// var koa = require('koa');
-// var controller = require('koa-route');
-// var app = koa();
-// var querystring = require('querystring');
-//
-// var views = require('co-views');
-// var render = views('./view', {
-//     map: {html: 'ejs'}
-// });
-//
-// var koa_static = require('koa-static-server');
-//
-//
-//
-// app.use(koa_static({
-//     rootDir: './static/',
-//     rootPath: '/static/',
-//     maxage: 0
-// }));
-//
-// app.use(controller.get('/', function*() {
-//     this.set('Cache-Control', 'no-cache');
-//     this.body = yield render('index');
-// }));
-//
-//
-// app.listen(3000);
-// console.log('Koa server is started!');
+var koa = require('koa');
+var controller = require('koa-route');
+var app = koa();
+var querystring = require('querystring');
+
+var views = require('co-views');
+var render = views('./view', {
+    map: {html: 'ejs'}
+});
+
+var koa_static = require('koa-static-server');
+
+
+
+app.use(koa_static({
+    rootDir: './static/',
+    rootPath: '/static/',
+    maxage: 0
+}));
+
+app.use(controller.get('/', function*() {
+    this.set('Cache-Control', 'no-cache');
+    this.body = yield render('index');
+}));
+
+
+app.listen(3000);
+console.log('Koa server is started!');
